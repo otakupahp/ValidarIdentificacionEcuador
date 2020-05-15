4f0d7176bf49c8cf1f15d82a82b642fd7e262d5a
PAHPDESKTOP\Pablo <pablo@otakupahp.com>
2020-05-15 05:53:08 -0500

    Añadida versión modificada del código original para funcionar como una librería de WordPress, así como la documentación para usarse dentro de un repositorio GIT

--- README.md
+++ README.md
@@ -1,2 +1,160 @@
-# ValidarIdentificacionEcuador
-Validador de identificaciones nacionales e internaiconales para Ecuador, basado en el código de Diaspar
+# Librería WordPress para validar Identificaciones en Ecuador
+
+Librería para WordPress que valida identificaciones nacionales e internacionales para Ecuador, basado en el código de [Diaspar](https://github.com/diaspar/validacion-cedula-ruc-ecuador)
+
+## Configurando la librería
+
+Para configurar el SDK de licencia como una librería:
+
+ 1. Incluir la base de código de la librería
+ 2. Cargar la librería incluyendo el archivo validador.php
+
+### 1. Incluir la base de código
+
+El uso de un subárbol en su complemento para incluir el SDK de licencia es el método recomendado.
+
+#### Paso 1. Agregar el repositorio como remoto
+
+```
+git remote add -f subtree-validador-identificacion https://github.com/otakupahp/validar-identificacion-ecuador.git
+```
+
+Agregar el subárbol como control remoto nos permite referirnos a él de forma breve a través del nombre subtree-validador-identificacion, en lugar de la URL completa de GitHub.
+
+#### Paso 2. Agregar el repositorio como un subárbol
+
+```
+git subtree add --prefix librerías/validador-identificacion subtree-validador-identificacion master --squash
+```
+
+Esto agregará la rama maestra del validador a su repositorio en la carpeta librerías/validador-identificacion.
+
+Se puede cambiar --prefix para apuntar donde se incluye el código.
+
+#### Paso 3. Actualizar el subárbol
+
+Para actualizar la librería a una nueva versión, se debe usar los siguientes comandos:
+
+```
+git fetch subtree-validador-identificacion master
+git subtree pull --prefix librerías/validador-identificacion subtree-validador-identificacion master --squash
+```
+
+### 2. Cargando la librería
+
+Independientemente de cómo se instale, para cargar la librería, solo se necesita incluir el archivo validador.php.
+
+```
+<? php
+require_once (plugin_dir_path (__FILE__). '/librerías/validador-identificacion/validador.php
+```
+
+## Uso de la librería
+
+Una vez cargada la librearía se deberá crear una instancia de la misma para poder usarla. 
+
+```
+$validador = new Validador('mi-plugin');
+```
+
+La variable que se envía es el dominio de "text-domain" usado para internacionalizar los mensajes del plugin.
+
+Para más información sobre internacionalización de plugins visitar la documentación de WordPress:
+
+ - [Internationalization](https://developer.wordpress.org/themes/functionality/internationalization/)
+ - [How to Internationalize Your Plugin](https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/)
+
+Con la instancia de la librería lista, ya se puede validar las identificaciones
+
+```
+# validar CI
+if ($validador->validarCedula('0926687856')) {
+    echo 'Cédula válida';
+} else {
+    echo 'Cédula incorrecta: '.$validador->getMessage();
+}
+
+# validar RUC persona natural
+if ($validador->validarRucPersonaNatural('0926687856001')) {
+    echo 'RUC válido';
+} else {
+    echo 'RUC incorrecto: '.$validador->getMessage();
+}
+
+# validar RUC sociedad privada
+if ($validador->validarRucSociedadPrivada('0992397535001')) {
+    echo 'RUC válido';
+} else {
+    echo 'RUC incorrecto: '.$validador->getMessage();
+}
+
+# validar RUC sociedad ublica
+if ($validador->validarRucSociedadPublica('1760001550001')) {
+    echo 'RUC válido';
+} else {
+    echo 'RUC incorrecto: '.$validador->getMessage();
+}
+```
+
+### Ejemplo: Función que comprueba si un valor dado es una cédula, RUC u otro documento
+
+A continuación un ejemplo de una función que se puede usar para comprobar si la identificación dada es del tipo esperado.
+
+```
+/**
+ * Validar si la identificación dada es una Cédula, RUC
+ *
+ * @param string $tipo  Tipo de documento a validar, los valores válidos son "cedula", "ruc"
+ * @param string $valor Valor a ser evaluado
+ 
+ * @throws exception    Emite un error si el valor no es del tipo esperado
+ */
+function validar_identificacion($tipo, $valor) {
+
+    # Text domain del plugin usado para la internacionalización
+    $plugin_name = 'mi-plugin'; 
+
+    # Si el tipo es cédula o ruc, validar
+    # cualquier otro se entiende como pasaporte o documento del exterior y no necesita validación 
+    if (in_array($tipo, [ 'cedula', 'ruc' ] )) {
+
+        require_once (plugin_dir_path (__FILE__). '/librerías/validador-identificacion/validador.php
+        $validador = new Validador();
+        $len = strlen($valor);
+
+        # Si la longitud de los datos es igual a 10, verificar una cédula
+        if ($len == 10 && $tipo == 'cedula') {
+            if (!$validador->validar_cedula($valor)) {
+                throw new Exception( sprintf( __( 'Cédula incorrecta: %s', $plugin_name ), $validador->get_error() ) );
+            }
+        }
+        # Si la longitud de los datos es igual a 13, verificar un ruc
+        elseif ($len == 13 && $tipo == 'ruc') {
+            if (
+                !(
+                    $validador->validar_ruc_persona_natural($valor) || 
+                    $validador->validar_ruc_sociedad_privada($valor) || 
+                    $validador->validar_ruc_sociedad_publica($valor)
+                )
+            ) {
+                throw new Exception( sprintf( __( 'RUC incorrecto: %s', $plugin_name ), $validador->get_error() ) );
+            }
+        }
+        # Si la longitud es diferente, verificar el tipo enviado para enviar mensaje de error
+        elseif ($tipo == 'cedula') {
+            throw new Exception( __( 'Ingresa una cédula correcta', $plugin_name ) );
+        } elseif ($tipo == 'ruc') {
+            throw new Exception( __( 'Ingresa un ruc correcto', $plugin_name ) );
+        }
+    }
+}
+
+# Al llamar la función, se deberá comprobar si existe un mensaje de error o no
+try {
+    validar_identificacion('cedula', '0926687856');
+    echo __( 'Identificación válida', 'mi-plugin' );
+}
+catch (Exception $exception) {
+    echo sprintf( __( 'Identificación inválida > %s', 'mi-plugin' ), $exception->getMessage() );
+}
+```  
---
+++ validador.php
@@ -0,0 +1,484 @@
+<?php
+/**
+ * MIT License
+ * ===========
+ *
+ * Copyright (c) 2012 Ing. Mauricio Lopez <mlopez@dixian.info>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @package     validador
+ * @subpackage
+ * @author      Ing. Mauricio Lopez <mlopez@dixian.info>
+ * @copyright   2012 Ing. Mauricio Lopez (diaspar)
+ * @license     http://www.opensource.org/licenses/mit-license.php  MIT License
+ * @link        http://www.dixian.info
+ * @version     @@0.8@@
+ */
+
+if( !class_exists('Validador') ) {
+
+    /**
+     * Validador contiene métodos para validar cédula, RUC de persona natural, RUC de sociedad privada y
+     * RUC de sociedad pública en el Ecuador.
+     *
+     * Los métodos públicos para realizar validaciones son:
+     *
+     * validar_identificacion()
+     * validar_cedula()
+     * validar_ruc_persona_natural()
+     * validar_ruc_sociedad_privada()
+     */
+    final class Validador
+    {
+
+        /**
+         * Error
+         *
+         * Contiene errores globales de la clase
+         *
+         * @var string
+         * @access protected
+         */
+        private $error = '';
+
+        /**
+         * The text domain used for internationalization
+         *
+         * @since    2.0.0
+         * @access   protected
+         * @var      string $text_domain The string used to uniquely identify this plugin.
+         */
+        protected $text_domain;
+
+        /**
+         * Validador constructor.
+         *
+         * @param $text_domain
+         */
+        public function __construct($text_domain = 'wordpress') {
+            $this->text_domain = $text_domain;
+        }
+
+        /**
+         * Validar cédula
+         *
+         * @param string $numero Número de cédula
+         *
+         * @return Boolean
+         */
+        public function validar_cedula($numero = '')
+        {
+            # fuerzo parámetro de entrada a string
+            $numero = (string)$numero;
+
+            # borro por si acaso errores de llamadas anteriores.
+            $this->set_error('');
+
+            # validaciones
+            try {
+                $this->validar_inicial($numero, '10');
+                $this->validar_codigo_provincia(substr($numero, 0, 2));
+                $this->validar_tercer_digito($numero[2], 'cedula');
+                $this->algoritmo_modulo_10(substr($numero, 0, 9), $numero[9]);
+            }
+            catch (Exception $exception) {
+                $this->set_error($exception->getMessage());
+                return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * Validar RUC persona natural
+         *
+         * @param string $numero Número de RUC persona natural
+         *
+         * @return Boolean
+         */
+        public function validar_ruc_persona_natural($numero = '')
+        {
+            # fuerzo parámetro de entrada a string
+            $numero = (string)$numero;
+
+            # borro por si acaso errores de llamadas anteriores.
+            $this->set_error('');
+
+            # validaciones
+            try {
+                $this->validar_inicial($numero, '13');
+                $this->validar_codigo_provincia(substr($numero, 0, 2));
+                $this->validar_tercer_digito($numero[2], 'ruc_natural');
+                $this->validar_codigo_establecimiento(substr($numero, 10, 3));
+                $this->algoritmo_modulo_10(substr($numero, 0, 9), $numero[9]);
+            }
+            catch (Exception $exception) {
+                $this->set_error($exception->getMessage());
+                return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * Validar RUC sociedad privada
+         *
+         * @param string $numero Número de RUC sociedad privada
+         *
+         * @return Boolean
+         */
+        public function validar_ruc_sociedad_privada($numero = '')
+        {
+            # fuerzo parámetro de entrada a string
+            $numero = (string)$numero;
+
+            # borro por si acaso errores de llamadas anteriores.
+            $this->set_error('');
+
+            # validaciones
+            try {
+                $this->validar_inicial($numero, '13');
+                $this->validar_codigo_provincia(substr($numero, 0, 2));
+                $this->validar_tercer_digito($numero[2], 'ruc_privada');
+                $this->validar_codigo_establecimiento(substr($numero, 10, 3));
+                $this->algoritmo_modulo_11(substr($numero, 0, 9), $numero[9], 'ruc_privada');
+            }
+            catch (Exception $exception) {
+                $this->set_error($exception->getMessage());
+                return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * Validar RUC sociedad publica
+         *
+         * @param string $numero Número de RUC sociedad publica
+         *
+         * @return Boolean
+         */
+        public function validar_ruc_sociedad_publica($numero = '')
+        {
+            # fuerzo parámetro de entrada a string
+            $numero = (string)$numero;
+
+            # borro por si acaso errores de llamadas anteriores.
+            $this->set_error('');
+
+            # validaciones
+            try {
+                $this->validar_inicial($numero, '13');
+                $this->validar_codigo_provincia(substr($numero, 0, 2));
+                $this->validar_tercer_digito($numero[2], 'ruc_publica');
+                $this->validar_codigo_establecimiento(substr($numero, 9, 4));
+                $this->algoritmo_modulo_11(substr($numero, 0, 8), $numero[8], 'ruc_publica');
+            }
+            catch (Exception $exception) {
+                $this->set_error($exception->getMessage());
+                return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * Validaciones iniciales para CI y RUC
+         *
+         * @param string $numero CI o RUC
+         * @param int $caracteres Cantidad de caracteres requeridos
+         *
+         * @throws exception Cuando valor esta vacío, cuando no es dígito y
+         * cuando no tiene cantidad requerida de caracteres
+         */
+        private function validar_inicial($numero, $caracteres)
+        {
+            if (empty($numero)) {
+                throw new Exception( __('Valor no puede estar vacío', $this->text_domain) );
+            }
+
+            if (!ctype_digit($numero)) {
+                throw new Exception( __('Valor ingresado solo puede tener dígitos', $this->text_domain) );
+            }
+
+            if (strlen($numero) != $caracteres) {
+                throw new Exception( sprintf( __('Valor ingresado debe tener %d caracteres', $this->text_domain), $caracteres) );
+            }
+
+        }
+
+        /**
+         * Validación de código de provincia (dos primeros dígitos de CI/RUC)
+         *
+         * @param string $numero Dos primeros dígitos de CI/RUC
+         *
+         * @throws exception Cuando el código de provincia no esta entre 00 y 24
+         */
+        private function validar_codigo_provincia($numero)
+        {
+            if ($numero < 0 or $numero > 24) {
+                throw new Exception( __('Codigo de Provincia (dos primeros dígitos) no deben ser mayor a 24 ni menores a 0', $this->text_domain) );
+            }
+        }
+
+        /**
+         * Validación de tercer dígito
+         *
+         * Permite validad el tercer dígito del documento. Dependiendo
+         * del campo tipo (tipo de identificación) se realizan las validaciones.
+         * Los posibles valores del campo tipo son: cedula, ruc_natural, ruc_privada
+         *
+         * Para Cédulas y RUC de personas naturales el tercer dígito debe
+         * estar entre 0 y 5 (0,1,2,3,4,5)
+         *
+         * Para RUC de sociedades privadas el tercer dígito debe ser
+         * igual a 9.
+         *
+         * Para RUC de sociedades públicas el tercer dígito debe ser
+         * igual a 6.
+         *
+         * @param string $numero tercer dígito de CI/RUC
+         * @param string $tipo tipo de identificador
+         *
+         * @throws exception Cuando el tercer digito no es válido. El mensaje
+         * de error depende del tipo de Identificación.
+         */
+        private function validar_tercer_digito($numero, $tipo)
+        {
+            switch ($tipo) {
+                case 'cedula':
+                case 'ruc_natural':
+                    if ($numero < 0 or $numero > 5) {
+                        throw new Exception( __('Tercer dígito debe ser mayor o igual a 0 y menor a 6 para cédulas y RUC de persona natural', $this->text_domain) );
+                    }
+                    break;
+                case 'ruc_privada':
+                    if ($numero != 9) {
+                        throw new Exception( __('Tercer dígito debe ser igual a 9 para sociedades privadas', $this->text_domain) );
+                    }
+                    break;
+
+                case 'ruc_publica':
+                    if ($numero != 6) {
+                        throw new Exception( __('Tercer dígito debe ser igual a 6 para sociedades públicas', $this->text_domain) );
+                    }
+                    break;
+                default:
+                    throw new Exception( __('Tipo de Identificación no existe.', $this->text_domain) );
+                    break;
+            }
+        }
+
+        /**
+         * Validación de código de establecimiento
+         *
+         * @param string $numero tercer dígito de CI/RUC
+         *
+         * @throws exception Cuando el establecimiento es menor a 1
+         */
+        private function validar_codigo_establecimiento($numero)
+        {
+            if ($numero < 1) {
+                throw new Exception( __('Código de establecimiento no puede ser 0', $this->text_domain) );
+            }
+        }
+
+        /**
+         * Algoritmo Modulo10 para validar si CI y RUC de persona natural son válidos.
+         *
+         * Los coeficientes usados para verificar el décimo dígito de la cédula,
+         * mediante el algoritmo “Módulo 10” son:  2. 1. 2. 1. 2. 1. 2. 1. 2
+         *
+         * Paso 1: Multiplicar cada dígito de los digitosIniciales por su respectivo
+         * coeficiente.
+         *
+         *  Ejemplo
+         *  digitosIniciales posición 1  x 2
+         *  digitosIniciales posición 2  x 1
+         *  digitosIniciales posición 3  x 2
+         *  digitosIniciales posición 4  x 1
+         *  digitosIniciales posición 5  x 2
+         *  digitosIniciales posición 6  x 1
+         *  digitosIniciales posición 7  x 2
+         *  digitosIniciales posición 8  x 1
+         *  digitosIniciales posición 9  x 2
+         *
+         * Paso 2: Sí alguno de los resultados de cada multiplicación es mayor a o igual a 10,
+         * se suma entre ambos dígitos de dicho resultado. Ex. 12->1+2->3
+         *
+         * Paso 3: Se suman los resultados y se obtiene total
+         *
+         * Paso 4: Divido total para 10, se guarda residuo. Se resta 10 menos el residuo.
+         * El valor obtenido debe concordar con el digitoVerificador
+         *
+         * Nota: Cuando el residuo es cero(0) el dígito verificador debe ser 0.
+         *
+         * @param string $digitos_iniciales Nueve primeros dígitos de CI/RUC
+         * @param string $digito_verificador Décimo dígito de CI/RUC
+         *
+         * @throws exception Cuando los digitosIniciales no concuerdan contra
+         * el código verificador.
+         */
+        private function algoritmo_modulo_10($digitos_iniciales, $digito_verificador) {
+            $array_coeficientes = [2, 1, 2, 1, 2, 1, 2, 1, 2];
+
+            $digito_verificador = (int)$digito_verificador;
+            $digitos_iniciales = str_split($digitos_iniciales);
+
+            $total = 0;
+            foreach ($digitos_iniciales as $key => $value) {
+
+                $valor_posicion = ((int)$value * $array_coeficientes[$key]);
+
+                if ($valor_posicion >= 10) {
+                    $valor_posicion = str_split($valor_posicion);
+                    $valor_posicion = array_sum($valor_posicion);
+                    $valor_posicion = (int)$valor_posicion;
+                }
+
+                $total = $total + $valor_posicion;
+            }
+
+            $residuo = $total % 10;
+
+            if ($residuo == 0) {
+                $resultado = 0;
+            }
+            else {
+                $resultado = 10 - $residuo;
+            }
+
+            if ($resultado != $digito_verificador) {
+                throw new Exception( __('Dígitos iniciales no validan contra Dígito Identificador', $this->text_domain) );
+            }
+
+        }
+
+        /**
+         * Algoritmo Modulo11 para validar RUC de sociedades privadas y públicas
+         *
+         * El código verificador es el décimo digito para RUC de empresas privadas
+         * y el noveno dígito para RUC de empresas públicas
+         *
+         * Paso 1: Multiplicar cada dígito de los digitosIniciales por su respectivo
+         * coeficiente.
+         *
+         * Para RUC privadas el coeficiente esta definido y se multiplica con las siguientes
+         * posiciones del RUC:
+         *
+         *  Ejemplo
+         *  digitosIniciales posición 1  x 4
+         *  digitosIniciales posición 2  x 3
+         *  digitosIniciales posición 3  x 2
+         *  digitosIniciales posición 4  x 7
+         *  digitosIniciales posición 5  x 6
+         *  digitosIniciales posición 6  x 5
+         *  digitosIniciales posición 7  x 4
+         *  digitosIniciales posición 8  x 3
+         *  digitosIniciales posición 9  x 2
+         *
+         * Para RUC privadas el coeficiente esta definido y se multiplica con las siguientes
+         * posiciones del RUC:
+         *
+         *  digitosIniciales posición 1  x 3
+         *  digitosIniciales posición 2  x 2
+         *  digitosIniciales posición 3  x 7
+         *  digitosIniciales posición 4  x 6
+         *  digitosIniciales posición 5  x 5
+         *  digitosIniciales posición 6  x 4
+         *  digitosIniciales posición 7  x 3
+         *  digitosIniciales posición 8  x 2
+         *
+         * Paso 2: Se suman los resultados y se obtiene total
+         *
+         * Paso 3: Divido total para 11, se guarda residuo. Se resta 11 menos el residuo.
+         * El valor obtenido debe concordar con el digitoVerificador
+         *
+         * Nota: Cuando el residuo es cero(0) el dígito verificador debe ser 0.
+         *
+         * @param string $digitos_iniciales Nueve primeros dígitos de RUC
+         * @param string $digito_verificador Décimo dígito de RUC
+         * @param string $tipo Tipo de identificador
+         *
+         * @throws exception Cuando los digitosIniciales no concuerdan contra
+         * el código verificador.
+         */
+        private function algoritmo_modulo_11($digitos_iniciales, $digito_verificador, $tipo)
+        {
+            switch ($tipo) {
+                case 'ruc_privada':
+                    $array_coeficientes = [4, 3, 2, 7, 6, 5, 4, 3, 2];
+                    break;
+                case 'ruc_publica':
+                    $array_coeficientes = [3, 2, 7, 6, 5, 4, 3, 2];
+                    break;
+                default:
+                    throw new Exception('Tipo de Identificación no existe.');
+                    break;
+            }
+
+            $digito_verificador = (int)$digito_verificador;
+            $digitos_iniciales = str_split($digitos_iniciales);
+
+            $total = 0;
+            foreach ($digitos_iniciales as $key => $value) {
+                $valor_posicion = ((int)$value * $array_coeficientes[$key]);
+                $total = $total + $valor_posicion;
+            }
+
+            $residuo = $total % 11;
+
+            if ($residuo == 0) {
+                $resultado = 0;
+            }
+            else {
+                $resultado = 11 - $residuo;
+            }
+
+            if ($resultado != $digito_verificador) {
+                throw new Exception( __('Dígitos iniciales no validan contra Dígito Idenficador', $this->text_domain) );
+            }
+        }
+
+        /**
+         * Get error
+         *
+         * @return string Mensaje de error
+         */
+        public function get_error()
+        {
+            return $this->error;
+        }
+
+        /**
+         * Set error
+         *
+         * @param string $newError
+         * @return object $this
+         */
+        public function set_error($newError)
+        {
+            $this->error = $newError;
+            return $this;
+        }
+    }
+
+}
